a26c2c7190a26635a17d209a153624fa9a3898f7
Kevin Scott <kevin@hitheory.com>
2018-10-24 22:09:43 -0400

    Set up example showing moons in action

---
+++ example/index.js
@@ -0,0 +1,26 @@
+/* globals Chart */
+import make_moons from '../src/index';
+const color = Chart.helpers.color;
+const NOISE = 0.1;
+const SAMPLES = 200;
+const data = {
+  datasets: [
+    {
+      label: 'Red',
+      borderColor: 'red',
+      backgroundColor: color('red').alpha(0.2).rgbString(),
+      data: make_moons({ type: 'pos', n_samples: SAMPLES, noise: NOISE }),
+    },
+    {
+      label: 'Blue',
+      borderColor: 'blue',
+      backgroundColor: color('blue').alpha(0.2).rgbString(),
+      data: make_moons({ type: 'neg', n_samples: SAMPLES, noise: NOISE }),
+    },
+  ],
+};
+
+const ctx = document.getElementById('chart').getContext('2d');
+Chart.Scatter(ctx, {
+  data,
+});
